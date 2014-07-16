153b36a2bc774eb31d48a42b531ae52a3261a1aa
Seth Fitzsimmons <seth@mojodna.net>
2014-07-16 16:49:07 -0700

    xsqst

---
+++ README.md
@@ -0,0 +1,116 @@
+# exquisite
+
+[SQS](http://aws.amazon.com/sqs/)-powered offline tasks. xsqst.
+
+## Usage
+
+```javascript
+var exquisite = require("exquite");
+
+var worker = exquisite({
+  name: "test" // queue name; if a queue does not exist with this name it will
+               // be created
+}, function(task, callback) {
+  console.log("worker #1:", task);
+
+  // extend the reservation on the current task by 30s
+  this.extend(30);
+
+  return setTimeout(callback, 5000);
+});
+
+// stop a worker from receiving new tasks
+worker.cancel();
+
+// start up a 2nd consumer
+exquisite({
+  name: "test" // queue name; if a queue does not exist with this name it will
+               // be created
+}, function(task, callback) {
+  console.log("worker #2:", task);
+
+  return setTimeout(callback, 2500);
+});
+
+//
+// queue management functions are also exposed
+//
+
+// get the current status of the queue (NOTE: these are approximate values)
+worker.queue.getLength(function(err, total, active) {});
+
+// queue a task
+worker.queue.queueTask({
+  foo: "bar"
+}, {
+  maxAttempts: 2
+}, function(err) {});
+
+// delete the queue
+worker.queue.delete(function(err) {});
+```
+
+A worker has 30s by default to complete a task before it becomes available to
+another worker. If you know that it will take longer, use `this.extend()`
+within the worker function to extend your exclusivity on a task. Calling this
+repeatedly will add the extension to the visibility timeout. If a task fails
+and is retried, its visibility timeout will be reset.  (This uses SQS's
+`VisibilityTimeout` attribute internally.)
+
+Tasks will be re-queued when the worker's callback is passed an `Error` (as the
+first argument) or when the visibility timeout expires. Tasks will be executed
+up to `maxAttempts` times (which defaults to `1`).  If your worker experiences
+a reproducible error when processing a task, your best option is to mark the
+task as complete and log the error elsewhere rather than continuing to let it
+fail.
+
+To manage a queue:
+
+```javascript
+var queue = require("exquisite")({
+  name: "test"
+}).queue;
+
+setInterval(function() {
+  queue.getLength(function(err, total, active) {
+    console.log("Queue length: %d (%d active)", total, active);
+  });
+}, 1000).unref();
+
+var taskCount = 50;
+var status = setInterval(function() {
+  if (taskCount-- > 0) {
+    queue.queueTask({
+      foo: "bar",
+      date: new Date()
+    }, {
+      maxAttempts: 2
+    });
+  } else {
+    clearInterval(status);
+  }
+}, 100);
+
+process.on("SIGINT", function() {
+  console.log("Deleting queue");
+  clearInterval(status);
+  queue.delete(process.exit);
+});
+```
+
+## Configuration
+
+### AWS
+
+exquisite uses [aws-sdk](http://aws.amazon.com/javascript/) under the hood, so
+all [configuration
+methods](http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html)
+for that also apply here. We typically use environment variables (see below).
+
+## Environment Variables
+
+* `AWS_ACCESS_KEY_ID` - An AWS access key with sufficient permission to create
+  and delete queues as well as send, receive, and update messages (specifically
+  visibility).
+* `AWS_SECRET_ACCESS_KEY` - Secret access key.
+* `AWS_DEFAULT_REGION` - AWS region to use. Defaults to `us-east-1`.
---
+++ index.js
@@ -0,0 +1,248 @@
+"use strict";
+
+var assert = require("assert"),
+    stream = require("stream"),
+    util = require("util");
+
+var _ = require("highland"),
+    AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: process.env.AWS_DEFAULT_REGION || "us-east-1"
+});
+
+var sqs = new AWS.SQS();
+
+var Worker = function(fn) {
+  stream.Writable.call(this, {
+    objectMode: true,
+    highWaterMark: 1 // limit the number of buffered tasks
+  });
+
+  this._write = function(task, _, callback) {
+    var payload = task.data;
+
+    // allow workers to mark a task as known to be in progress for an estimate
+    // future duration
+    var ctx = {
+      extend: function(time) {
+        sqs.changeMessageVisibility({
+          QueueUrl: task.queueUrl,
+          ReceiptHandle: task.receiptHandle,
+          VisibilityTimeout: time
+        }, function(err) {
+          if (err) {
+            console.warn(err.stack);
+          }
+        });
+      }
+    };
+
+    return fn.bind(ctx)(payload, function(err) {
+      if (err) {
+        console.warn(err.stack);
+
+        // update visibility so it can be retried
+        sqs.changeMessageVisibility({
+          QueueUrl: task.queueUrl,
+          ReceiptHandle: task.receiptHandle,
+          VisibilityTimeout: 0
+        }, function(err) {
+          if (err) {
+            console.warn(err.stack);
+          }
+        });
+      } else {
+        // delete task
+        sqs.deleteMessage({
+          QueueUrl: task.queueUrl,
+          ReceiptHandle: task.receiptHandle
+        }, function(err) {
+          if (err) {
+            console.warn(err.stack);
+          }
+        });
+      }
+
+      return callback();
+    });
+  };
+};
+
+util.inherits(Worker, stream.Writable);
+
+/**
+ * Available options:
+ * * name - Queue name (required)
+ * * delay - Delay (in seconds) before queueing tasks. Defaults to 0.
+ * * visibilityTimeout - Max expected task time. Defaults to 30.
+ */
+module.exports = function(options, fn) {
+  assert.ok(options.name, "options.name is required");
+
+  var queueUrl;
+
+  sqs.createQueue({
+    QueueName: options.name,
+    Attributes: {
+      DelaySeconds: (options.delay || 0).toString(),
+      ReceiveMessageWaitTimeSeconds: "20",
+      VisibilityTimeout: (options.visibilityTimeout || 30).toString()
+    }
+  }, function(err, data) {
+    if (err) {
+      console.warn(err.stack);
+      return;
+    }
+
+    queueUrl = data.QueueUrl;
+  });
+
+  var getQueueUrl = function(callback) {
+    if (queueUrl) {
+      return callback(null, queueUrl);
+    }
+
+    return setImmediate(getQueueUrl, callback);
+  };
+
+  var source = _(function(push, next) {
+    return getQueueUrl(function(err, queueUrl) {
+      return sqs.receiveMessage({
+        QueueUrl: queueUrl,
+        MaxNumberOfMessages: 1,
+        AttributeNames: ["ApproximateFirstReceiveTimestamp",
+                         "ApproximateReceiveCount",
+                         "SentTimestamp"]
+      }, function(err, data) {
+        if (err) {
+          console.warn(err.stack);
+          return next();
+        }
+
+        if (data.Messages) {
+          data.Messages
+            .map(function(msg) {
+              var attempts = msg.Attributes.ApproximateReceiveCount | 0,
+                  payload;
+
+              try {
+                payload = JSON.parse(msg.Body);
+              } catch (err) {
+                console.warn(err);
+                return;
+              }
+
+              if (attempts > payload.maxAttempts) {
+                // fire and forget
+                sqs.deleteMessage({
+                  QueueUrl: queueUrl,
+                  ReceiptHandle: msg.ReceiptHandle
+                }, function(err) {
+                  if (err) {
+                    console.warn(err);
+                  }
+                });
+
+                return;
+              }
+
+              return {
+                queueUrl: queueUrl,
+                messageId: msg.MessageId,
+                receiptHandle: msg.ReceiptHandle,
+                attributes: msg.Attributes,
+                attempts: attempts,
+                data: payload.data
+              };
+            })
+            .filter(function(task) {
+              // filter out tasks that failed to parse
+              return !!task;
+            })
+            .forEach(function(task) {
+              push(null, task);
+            });
+        }
+
+        return next();
+      });
+    });
+  });
+
+  var queueTask = function(data, options, callback) {
+    options = options || {};
+    callback = callback || function(err) {
+      if (err) {
+        console.warn(err.stack);
+      }
+    };
+
+    var payload = {
+      maxAttempts: options.maxAttempts || 1,
+      data: data
+    };
+
+    return getQueueUrl(function(err, queueUrl) {
+      return sqs.sendMessage({
+        QueueUrl: queueUrl,
+        MessageBody: JSON.stringify(payload)
+      }, function(err) {
+        // client doesn't need to see the data
+        return callback(err);
+      });
+    });
+  };
+
+  var deleteQueue = function(callback) {
+    callback = callback || function(err) {
+      if (err) {
+        console.warn(err.stack);
+      }
+    };
+
+    return getQueueUrl(function(err, queueUrl) {
+      return sqs.deleteQueue({
+        QueueUrl: queueUrl
+      }, callback);
+    });
+  };
+
+  var getLength = function(callback) {
+    return getQueueUrl(function(err, queueUrl) {
+      return sqs.getQueueAttributes({
+        QueueUrl: queueUrl,
+        AttributeNames: ["ApproximateNumberOfMessages",
+                         "ApproximateNumberOfMessagesNotVisible"]
+      }, function(err, data) {
+        if (err) {
+          return callback(err);
+        }
+
+        return callback(null,
+                        data.Attributes.ApproximateNumberOfMessages,
+                        data.Attributes.ApproximateNumberOfMessagesNotVisible);
+      });
+    });
+  };
+
+  var retval = {
+    queue: {
+      delete: deleteQueue,
+      getLength: getLength,
+      queueTask: queueTask
+    }
+  };
+
+  if (fn) {
+    var worker = new Worker(fn);
+
+    source.pipe(worker);
+
+    retval.cancel = function() {
+      source.unpipe(worker);
+    };
+  }
+
+  return retval;
+};
